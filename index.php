df39b2e2338d71516242e0b4b83a8dbf05386865
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-25 22:28:32 +0200

    ajout d'un fichier header.php et footer.php

--- index.php
+++ index.php
@@ -8,19 +8,10 @@ $bd = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 var_dump($bd);
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-</head>
-<body>
 
+<?php include "header.php" ?>
+
+<main>
 <h1>Mes albums</h1>
 
       <?php foreach ($bd as $item) : ?>
@@ -40,5 +31,6 @@ var_dump($bd);
                   </div>
             </div>
       <?php endforeach; ?>
-</body>
-</html>
+</main>
+
+<?php include "footer.php" ?>
--- item.php
+++ item.php
@@ -11,16 +11,9 @@ $bd = $stmt->fetchAll(PDO::FETCH_ASSOC);
 var_dump($bd);
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
-</head>
-<body>
+<?php include "header.php" ?>
+<main>
       <img src="assets/images/<?php echo $bd[0]['image'] ?>">
-</body>
-</html>
+</main>
+
+<?php include "footer.php" ?>
